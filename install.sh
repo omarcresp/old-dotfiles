6fb1045faafa76025d83d6ecdf6baec1c3c21c56
Omar Crespo <crespomerchano@gmail.com>
2024-04-04 12:40:32 -0500

    Fix lazygit instalation

--- install.sh
+++ install.sh
@@ -174,7 +174,8 @@ else
 
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    tar xf /tmp/lazygit.tar.gz /tmp/lazygit
+    tar xf /tmp/lazygit.tar.gz lazygit
+    mv lazygit /tmp/lazygit
     sudo install /tmp/lazygit /usr/local/bin
 
     echo "=== JACKCRES dotfiles: lazygit installed"
